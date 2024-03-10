83c3d2d0d8775fb3ba0ebd1617d156e6e5b97480
Lukas Hartrumpf <lukas.hartrumpf@nerdware.dev>
2024-03-10 08:40:06 +0100

    cleanup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,17 +2,53 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-
 import {
   DynamoDBDocumentClient,
   GetCommand,
   PutCommand,
 } from "@aws-sdk/lib-dynamodb";
 
+config();
+
+if (!process.env.TABLE || !process.env.TENANT) {
+  console.error("Missing TABLE or TENANT environment variable");
+  process.exit(1);
+}
+
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-config();
+const putItem = async () => {
+  const params = {
+    TableName: process.env.TABLE,
+    Item: {
+      Tenant: process.env.TENANT,
+    },
+  };
+
+  try {
+    await ddbDocClient.send(new PutCommand(params));
+    console.log("Item put into DynamoDB table");
+  } catch (error) {
+    console.error("Error putting item into DynamoDB table", error);
+  }
+};
+
+const getItem = async () => {
+  const params = {
+    TableName: process.env.TABLE,
+    Key: {
+      Tenant: process.env.TENANT,
+    },
+  };
+
+  try {
+    return await ddbDocClient.send(new GetCommand(params));
+  } catch (error) {
+    console.error("Error getting item from DynamoDB table", error);
+    throw error;
+  }
+};
 
 const app = express();
 
@@ -24,33 +60,13 @@ app.use(
 );
 
 // Put an item into the DynamoDB table on server start for seeding
-const putParams = {
-  TableName: process.env.TABLE,
-  Item: {
-    Tenant: process.env.TENANT,
-  },
-};
-
-ddbDocClient
-  .send(new PutCommand(putParams))
-  .then(() => console.log("Item put into DynamoDB table"))
-  .catch((error) =>
-    console.error("Error putting item into DynamoDB table", error)
-  );
+putItem();
 
 app.get("/tenant", async (_: Request, res: Response) => {
-  const params = {
-    TableName: process.env.TABLE,
-    Key: {
-      Tenant: process.env.TENANT,
-    },
-  };
-
   try {
-    const data = await ddbDocClient.send(new GetCommand(params));
+    const data = await getItem();
     res.send(data.Item?.Tenant.toUpperCase() ?? "UNKNOWN");
   } catch (error) {
-    console.error(error);
     res.status(500).send("Error retrieving tenant");
   }
 });
